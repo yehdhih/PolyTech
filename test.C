4c98631fa05524e81a0fb7c13c1eb65f2d59c8e2
yehdhih <yommoctar@gmail.com>
2017-09-05 17:46:55 -0500

    Added code to shown effect of left shift on int values

--- test.C
+++ test.C
@@ -2,5 +2,13 @@
 #include <stdlib.h>
 
 int main ( ) {
-  printf("C is alive and well ...");
-}
+  int x = 8;
+  int N = 4;
+  printf (" Initial value of x = %d\n", x);
+  for ( int i = 0; i < N; i++ ) {
+    x = x << 1;
+    printf("New value of x = %d\n", x);
+  }
+  printf("C is alive and well ...\n");
+
+} 
