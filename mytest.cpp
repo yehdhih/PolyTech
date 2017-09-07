97048bb6cc54ddb6ee6f56d8d8539905074b9cb6
yehdhih <yommoctar@gmail.com>
2017-09-07 15:45:10 -0500

    Added code to make mytest.cpp works in C++ mode

--- mytest.cpp
+++ mytest.cpp
@@ -1,1 +1,10 @@
-printf("today is the 7 th of septembre \n");
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+  printf("today is the 7 th of septembre \n");
+  cout << "This is C++ style printf" << endl;
+}
